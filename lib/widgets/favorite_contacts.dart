1eed9de322e23a0cbe98b4f74de4faea72299399
Abraham Kazerian <abrahamyaoming@gmail.com>
2020-02-19 22:50:52 -0300

    Another missing file

--- lib/widgets/favorite_contacts.dart
+++ lib/widgets/favorite_contacts.dart
@@ -36,53 +36,53 @@ class FavoriteContacts extends StatelessWidget {
               ],
             ),
           ),
-          Container(
-            height: 120,
-            child: ListView.builder(
-              scrollDirection: Axis.horizontal,
-              padding: EdgeInsets.only(left: 10),
-              itemCount: favUsers.length,
-              itemBuilder: (BuildContext context, int index) {
-                return GestureDetector(
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (_) => ChatScreen(
-                          user: favUsers[index],
-                        ),
-                      ),
-                    );
-                  },
-                  child: Padding(
-                    padding: EdgeInsets.all(10.0),
-                    child: Column(
-                      children: <Widget>[
-                        CircleAvatar(
-                          radius: 35.0,
-                          backgroundColor: Colors.grey,
-                          backgroundImage: AssetImage(
-                            favUsers[index].imageUrl,
-                          ),
-                        ),
-                        SizedBox(
-                          height: 6.0,
-                        ),
-                        Text(
-                          favUsers[index].name,
-                          style: TextStyle(
-                            color: Colors.blueGrey,
-                            fontSize: 16.0,
-                            fontWeight: FontWeight.w600,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                );
-              },
-            ),
-          ),
+          // Container(
+          //   height: 120,
+          //   child: ListView.builder(
+          //     scrollDirection: Axis.horizontal,
+          //     padding: EdgeInsets.only(left: 10),
+          //     itemCount: favUsers.length,
+          //     itemBuilder: (BuildContext context, int index) {
+          //       return GestureDetector(
+          //         onTap: () {
+          //           Navigator.push(
+          //             context,
+          //             MaterialPageRoute(
+          //               builder: (_) => ChatScreen(
+          //                 user: favUsers[index],
+          //               ),
+          //             ),
+          //           );
+          //         },
+          //         child: Padding(
+          //           padding: EdgeInsets.all(10.0),
+          //           child: Column(
+          //             children: <Widget>[
+          //               CircleAvatar(
+          //                 radius: 35.0,
+          //                 backgroundColor: Colors.grey,
+          //                 backgroundImage: AssetImage(
+          //                   favUsers[index].imageUrl,
+          //                 ),
+          //               ),
+          //               SizedBox(
+          //                 height: 6.0,
+          //               ),
+          //               Text(
+          //                 favUsers[index].name,
+          //                 style: TextStyle(
+          //                   color: Colors.blueGrey,
+          //                   fontSize: 16.0,
+          //                   fontWeight: FontWeight.w600,
+          //                 ),
+          //               ),
+          //             ],
+          //           ),
+          //         ),
+          //       );
+          //     },
+          //   ),
+          // ),
         ],
       ),
     );
